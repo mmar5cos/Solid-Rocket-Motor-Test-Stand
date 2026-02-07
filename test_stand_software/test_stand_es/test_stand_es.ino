1781372b4b008810e8fa44e3e9ad4ad3e44b2324
Fredy Orellana <feorella@Fredys-MacBook-Air-2.local>
2026-02-06 18:29:18 -0600

    got bluetooth working with two xbees

--- test_stand_software/test_stand_es/test_stand_es.ino
+++ test_stand_software/test_stand_es/test_stand_es.ino
@@ -1,34 +1,28 @@
 #include <SoftwareSerial.h>
 
-
-// RX, TX
-SoftwareSerial HM10(2, 3);
-
 #define TEST_STAND_HZ 50
-#define LOOP_DELAY_MS (1000 / TEST_STAND_HZ) 
+#define LOOP_DELAY_MS (1000 / TEST_STAND_HZ)
 
+// XBee: Arduino RX=D9 (from XBee DOUT), Arduino TX=D8 (to XBee DIN, level-shift!)
+SoftwareSerial xbee(9, 8);
 
 void setup() 
 {
-  Serial.begin(115200);   // USB serial for debugging
-  HM10.begin(115200);       // HM-10 default baud rate
+  Serial.begin(115200);   // USB to Mac (your Python script)
+  xbee.begin(115200);       // MUCH more reliable on Uno SoftwareSerial than 115200
 
-  Serial.println("HM-10 Echo Ready");
+  Serial.println("XBee/USB Echo Ready");
 }
-
 
 void loop() 
 {
-  
-  // If HM-10 sends data → echo it back
-  if (HM10.available()) 
-  {
-    char c = HM10.read();
 
-    HM10.write(c);    // send back to Bluetooth device
-    Serial.write(c);  // also show on Serial Monitor (optional)
+  // ---- USB Serial (Mac) -> echo back to Mac ----
+  while (xbee.available()) 
+  {
+    char c = (char)xbee.read();
+    xbee.write(c);       // send back over wireless
   }
 
   delay(LOOP_DELAY_MS);
-
 }
